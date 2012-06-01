5a847890993d661afea89ae6a699c21142abe0f4
Thomas W <thomas@matchstrike.net>
2012-06-01 00:41:30 -0700

    Allow the index view to grab and return images from dropbox

--- moisinator/base.py
+++ moisinator/base.py
@@ -1,11 +1,24 @@
 import os
 
+import requests
 import web
 
-from settings import SITE_ROOT
+from settings import SITE_ROOT, DROPBOX_BASE_URL
 
 render = web.template.render(os.path.join(SITE_ROOT, 'templates'))
 
 class Index:
-    def GET(self):
-        return render.index()
+    def GET(self, image):
+        if not image:
+            return render.index()
+        
+        return self.render_image("%s%s" % (DROPBOX_BASE_URL, image))
+    
+    def render_image(self, image):
+        image = requests.get(image)
+        image.is_image = image.headers['content-type'].startswith('image')
+        
+        if image.status_code != 200 and not image.is_image:
+            raise web.notfound()
+        
+        return image.content
--- moisinator/settings.py
+++ moisinator/settings.py
@@ -1,3 +1,5 @@
 import os
 
 SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+
+DROPBOX_BASE_URL = "https://photos.dropbox.com/"
